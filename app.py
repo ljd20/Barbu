c0f96a28c23b0f8150263aaab891096a7f7e58c6
Louise <ljd20@ic.ac.uk>
2024-07-25 18:09:17 +0100

    waitress

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 from itertools import product
 from random import shuffle
 import os
+import waitress
 
 app = Flask(__name__)
 CORS(app)
@@ -308,5 +309,5 @@ def handle_claim_cards(data):
     socketio.emit('update_center_cards', {'center_cards': game.Rounds.center_cards}, room='game_room')
 
 if __name__ == '__main__':
-    app.run()  # This line can be removed in production
-
+    # Use waitress to serve the app
+    waitress.serve(app, host='0.0.0.0', port=5000)
