11e4d827b9ca3d0d91d7d8099568a05ee5aaa4b9
Louise <ljd20@ic.ac.uk>
2024-07-25 17:31:46 +0100

    change web

--- app.py
+++ app.py
@@ -308,5 +308,4 @@ def handle_claim_cards(data):
     socketio.emit('update_center_cards', {'center_cards': game.Rounds.center_cards}, room='game_room')
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    app.run()
